b46d7145803cdb378efa51bbe5c9771203d68cbe
Sergey Kuleshov <svyatogor@gmail.com>
2017-11-25 12:59:18 +0200

    fix t function

--- common/utils.js
+++ common/utils.js
@@ -1,3 +1,8 @@
+import {isString} from 'lodash'
+
 export function t(i18nString, locale, fallback) {
+  if (isString(i18nString)) {
+    return i18nString
+  }
   return i18nString && (i18nString[locale] || i18nString[fallback||'en'] || '')
 }
