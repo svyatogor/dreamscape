9d5564a287d659428d218a22adfb7b516236695d
Sergey Kuleshov <svyatogor@gmail.com>
2018-02-03 23:34:05 +0200

    support for custom contact forms

--- backend/middlewares/contact_form.js
+++ backend/middlewares/contact_form.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {isEmpty} from 'lodash'
+import {get, isEmpty} from 'lodash'
 import url from 'url'
 import bodyParser from 'body-parser'
 import Joi from 'joi'
@@ -7,15 +7,20 @@ import {form as  joiToForms} from 'joi-errors-for-forms'
 import {mailTransporter} from '../common/mailer'
 import {renderRequest} from '../frontend'
 import {renderEmail} from '../renderers'
+import {builder as joiBuilder} from 'joi-json'
 
 export const contact_form = express()
 
+const defaultSchema =  {
+  name: {'@type': 'string', max: 250, required: true},
+  email: {'@type': 'email', required: true},
+  message: {'@type': 'string', required: true},
+}
 
-const schema = Joi.object().keys({
-    name: Joi.string().max(250).required(),
-    email: Joi.string().email().required(),
-    message: Joi.string().required()
-})
+const schema = (site, type = 'contact') => {
+  const schema = get(site, ['forms', type, 'fields'], defaultSchema)
+  return Joi.object().keys(joiBuilder().build(schema))
+}
 
 contact_form.post('/contact_form', bodyParser.urlencoded({extended: true}), (req, res, next) => {
   const ref = req.get('Referrer')
@@ -26,34 +31,36 @@ contact_form.post('/contact_form', bodyParser.urlencoded({extended: true}), (req
     path = '/'
   }
 
-  const {value, error} = Joi.validate(req.body.contact_form, schema, {abortEarly: false, stripUnknown: true})
+  const {site, body: {contact_form, form}} = req
+  const {value, error} = Joi.validate(contact_form, schema(site, form), {abortEarly: false, stripUnknown: true})
   const errorPath = req.body.callback_error || path
   const successPath = req.body.callback_success || path
   if (error) {
     renderRequest(errorPath, {req, res, next}, {
       contact_form: {
-        ...req.body.contact_form,
+        ...contact_form,
         errors: joiToForms()(error),
         success: false,
         error,
       }
     })
   } else {
-    renderEmail(req, 'email', value).then(({body, subject}) => {
+    const template = get(site, ['forms', form, 'template'], 'email')
+    renderEmail(req, template, value).then(({body, subject}) => {
       mailTransporter.sendMail({
         from: "noreply@dreamscape.tech",
         // replyTo: {
         //   name: value.name,
         //   address: value.email,
         // },
-        to: req.site.notificationEmail,
+        to: site.notificationEmail,
         subject,
         html: body,
       }, (err, info) => {
         if (err) {
           renderRequest(errorPath, {req, res, next}, {
             contact_form: {
-              ...req.body.contact_form,
+              ...contact_form,
               success: false,
               error: err,
             }
